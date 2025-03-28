608e21fa3406a8dc8aeca6676eb2c3c91c8009c0
Maryna Sakova <marynasakova@macbookpro.speedport.ip>
2025-03-28 13:30:42 +0100

    SearchBar.js was added, changes applied to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from "react";
 import BusinessList from './BusinessList';
+import SearchBar from "./SearchBar";
 
 
 function App() {
   return (
     <div className="App">
       <h1>Ravenous</h1>
+      <SearchBar />
       <BusinessList />
     </div>
   );
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,2 +1,34 @@
 import React from "react";
 
+const sortOptions = {
+    "Best Match": "best_match",
+    "Highest Rated": "rating",
+    "Most Reviewed": "review_count"
+  };
+
+function SearchBar() {
+    return(
+        <div className="search">
+            <div className="sortingOptions">
+            <ul>
+                {Object.entries(sortOptions).map(([key, value]) => (
+                    <li key={key}>{value}</li>
+                ))}
+            </ul>
+                {/*
+                <p id="bestMatch">Best Match</p>
+                <p id="highestRated">Highest Rated</p>
+                <p id="mostReviewd">Most reviewd</p>
+                */}
+            </div>
+            <div className="searchBar">
+                <input type="search" id="businessSearch" placeholder="Search Businesses" />
+                <input type="search" id="locationSearch" placeholder="Where?" />
+                <button>Let's Go</button>
+            </div>
+        </div>
+    );
+};
+
+
+export default SearchBar;
